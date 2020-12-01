7dc92a6fa431de8569bbf2943576566cfd60a9f6
kinsho-stb <tomoya.kinsho@stanby.inc>
2020-12-02 00:26:38 +0900

    add exercise6.10

--- scalaFunctional/src/main/scala/PureFunction.scala
+++ scalaFunctional/src/main/scala/PureFunction.scala
@@ -89,11 +89,71 @@ object PureFunction {
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
     rng => {
       val (a: A, r1: RNG) = ra(rng)
-      val (b: B, r2: RNG) = rb(rng)
+      val (b: B, r2: RNG) = rb(r1)
       (f(a, b), r2)
     }
   }
 
-  def both[A, B]()
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  // exercise6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+
+    fs.foldRight(unit(List[A]()))((f: Rand[A], acc: Rand[List[A]]) => map2(f, acc)(_ :: _))
+
+//    rang => {
+//      fs match {
+//        case head :: tail => head(rang)
+//      }
+//    }
+  }
+
+  // exercise6.8
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+  }
+
+  // exercise6.8
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    })
+
+
+
+  // exercise6.9
+  def mapViaFlatMap[A, B](s: RNG => (A, RNG))(f: A => B): Rand[B] = {
+    flatMap(s)(a => unit(f(a)))
+  }
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
+  }
+
+}
+
+
+// exercise6.10
+object Test {
+  type Rand[A] = State[RNG, A]
+
+  case class State[S,+A](run: S => (A, S)) {
+    def unit(a: A): Rand[A] = {
+      State {
+        s: RNG => (a, s)
+      }
+    }
+
+
+
+
+  }
+
 
 }
