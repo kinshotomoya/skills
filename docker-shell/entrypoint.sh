be3c8e5da318dbc098b66895df3115a9d636850d
金正朋也 <mhtnim1023@gmail.com>
2023-06-21 01:53:29 +0900

    続き

--- docker-shell/entrypoint.sh
+++ docker-shell/entrypoint.sh
@@ -3,6 +3,14 @@
 
 handle_signal() {
     echo 'SIGINT/SIGTERMを検出しました。'
+    # メインプロセスの子プロセスID一覧を取得
+    local children=`ps --ppid $$ --no-heading | awk '{ print $1 }'`
+    for child in $children
+    do
+        echo $child
+        # 明示的に子プロセスを終了させる
+        kill –s TERM $child
+    done
     exit 0
 }
 
